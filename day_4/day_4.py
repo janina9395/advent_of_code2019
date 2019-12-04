b13b035a5f4bd00f2a3e2ebb41b9ebb786e32d76
ianina <yanina.km@gmail.com>
2019-12-04 22:27:02 +0100

    Days 3 and 4

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,9 +1,27 @@
+def to_digits(n):
+    return list(map(int, list(str(n))))
+
 def check_number(n):
-    digits = list(map(lambda x: int(x), list(str(n))))
+    digits = to_digits(n)
     prev = digits[0]
     rest = digits[1:]
 
     double = False
+    increasing = True
+    for next in rest:
+        if next < prev:
+            increasing = False
+            break
+        if prev == next:
+            double = True
+        prev = next
+    return double and increasing
+
+def check_number_part2(n):
+    digits = to_digits(n)
+    prev = digits[0]
+    rest = digits[1:]
+
     increasing = True
     counters = {}
     for next in rest:
@@ -11,25 +29,29 @@ def check_number(n):
             increasing = False
             break
         if prev == next:
-            double = True
-            if prev in counters.keys():
-                counters[prev] = counters[prev] + 1
-            else:
-                counters[prev] = 2
+            counters[prev] = counters.get(prev, 1) + 1
         prev = next
 
     # print(counters)
-    return double and increasing and 2 in counters.values()
+    return increasing and 2 in counters.values()
 
+assert(check_number(122345) == True)
+assert(check_number(111111) == True)
+assert(check_number(223450) == False)
+assert(check_number(123789) == False)
 
-print(check_number(122345))
-print(check_number(111111))
-print(check_number(223450))
-print(check_number(123789))
+def count_matches(match_func):
+    count = 0
+    for n in range(193651, 649729):
+        if match_func(n):
+            count += 1
+    return count
 
-count = 0
-for n in range(193651, 649729):
-    if check_number(n):
-        count += 1
+print("Part 1:", count_matches(check_number))
 
-print(count)
+assert(check_number_part2(122345) == True)
+assert(check_number_part2(111111) == False)
+assert(check_number_part2(223450) == False)
+assert(check_number_part2(123789) == False)
+
+print("Part 2:", count_matches(check_number_part2))
